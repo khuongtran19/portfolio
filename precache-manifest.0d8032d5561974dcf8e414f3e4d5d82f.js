088c84334953040ded241ee2397752274bb63d6f
khuongtran <tranvinhkhuong@gmail.com>
2019-01-18 14:13:25 -0800

    Updates

---
+++ precache-manifest.0d8032d5561974dcf8e414f3e4d5d82f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b22a2ed6d4d4842ef0e0e9179378496",
+    "url": "/portfolio/static/media/gif1.2b22a2ed.gif"
+  },
+  {
+    "revision": "b86f9dc3cffb6a8eeebf",
+    "url": "/portfolio/static/css/main.64ecc15e.chunk.css"
+  },
+  {
+    "revision": "15413a89ded24bd12f4f2c258886b0b2",
+    "url": "/portfolio/static/media/wallpaper1.15413a89.jpg"
+  },
+  {
+    "revision": "a76d8aad86c6ae2edddb",
+    "url": "/portfolio/static/js/1.a76d8aad.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "5c3349b7df205a88169c52a49a82e4e5",
+    "url": "/portfolio/static/media/gif0.5c3349b7.gif"
+  },
+  {
+    "revision": "b86f9dc3cffb6a8eeebf",
+    "url": "/portfolio/static/js/main.b86f9dc3.chunk.js"
+  },
+  {
+    "revision": "6e7b7b255a34afdf3051a0f8f361fb53",
+    "url": "/portfolio/static/media/gif2.6e7b7b25.gif"
+  },
+  {
+    "revision": "8e45e4064a879016036277feac60c9a8",
+    "url": "/portfolio/static/media/gif3.8e45e406.gif"
+  },
+  {
+    "revision": "372b6a45f3b36413693482bda1bb7576",
+    "url": "/portfolio/static/media/gif4.372b6a45.gif"
+  },
+  {
+    "revision": "a76d8aad86c6ae2edddb",
+    "url": "/portfolio/static/css/1.ae5fe9ce.chunk.css"
+  },
+  {
+    "revision": "e04be9f5527461be053aa9afedbf14e1",
+    "url": "/portfolio/index.html"
+  }
+];
